867e43f129d5ce534df3456da60b188888fdd0d0
paulKlochkov <pavel.klochkow@gmail.com>
2014-01-22 02:34:56 +0200

    Signed-off-by: paulKlochkov <pavel.klochkow@gmail.com>
    Добавил ДАО

---
+++ src/by/pklochkov/jedi/dao/RoleDAO.java
@@ -0,0 +1,32 @@
+package by.pklochkov.jedi.dao;
+
+import by.pklochkov.jedi.entity.Role;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pklochkov
+ * Date: 21.01.14
+ * Time: 22:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class RoleDAO extends DAO<Role,Long> {
+    @Override
+    public Role create(Role object) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Role update(Role object) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Role delete(Role object) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Role getById(Long id) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
